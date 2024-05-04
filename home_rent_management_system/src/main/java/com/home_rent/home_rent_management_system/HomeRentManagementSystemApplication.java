cac776f27d356a5a18757717aec3dc3f6028a2a6
Mosiur99 <mosiurzzaman99@gmail.com>
2024-05-04 17:23:12 +0600

    project created

---
+++ home_rent_management_system/src/main/java/com/home_rent/home_rent_management_system/HomeRentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.home_rent.home_rent_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeRentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeRentManagementSystemApplication.class, args);
+	}
+
+}
---
+++ home_rent_management_system/src/test/java/com/home_rent/home_rent_management_system/HomeRentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home_rent.home_rent_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeRentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
