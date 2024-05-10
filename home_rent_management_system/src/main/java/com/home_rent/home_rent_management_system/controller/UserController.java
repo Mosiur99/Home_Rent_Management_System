498625e9e848b33caf26e61978021f6ea02e2e96
Mosiur99 <mosiurzzaman99@gmail.com>
2024-05-10 22:44:12 +0600

    district enum added

--- home_rent_management_system/src/main/java/com/home_rent/home_rent_management_system/controller/UserController.java
+++ home_rent_management_system/src/main/java/com/home_rent/home_rent_management_system/controller/UserController.java
@@ -4,6 +4,7 @@ import com.home_rent.home_rent_management_system.entity.enums.Division;
 import com.home_rent.home_rent_management_system.entity.enums.JobType;
 import com.home_rent.home_rent_management_system.entity.enums.UserType;
 import com.home_rent.home_rent_management_system.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -34,8 +35,10 @@ public class UserController {
                          @RequestParam String password,
                          @RequestParam String lastName,
                          @RequestParam String firstName,
-                         @RequestParam Division division) {
+                         @RequestParam Division division,
+                         HttpSession httpSession) {
         userService.signup(age, email, jobType, password, lastName, firstName, division);
+        httpSession.setAttribute("message", "User Register Successfully...");
         return "redirect:/signup";
     }
 }
